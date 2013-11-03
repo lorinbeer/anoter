0e42a0918bc7c0a659abb9408bc4cca1606e5fcd
lorinbeer <lorin@adobe.com>
2013-11-03 01:20:09 -0800

    more or less stable basics

--- js/index.js
+++ js/index.js
@@ -45,10 +45,37 @@ var app = {
     },
     
     onstartconfig: function() {
+       
         cblite.getURL(function(err,url) {
-            console.log("CBLITE");
-            var db = coax([url, appDbName]);           
-        });
+                        console.log("CBLITE");
+                        var db = coax([url, appDbName]);
+                        setupDb(db, function(err, info){
+                            console.log("setupDb: ", err, info);
+                              setupViews(db, function(err, views) {
+                                         //console.log(err, views);
+                                    window.config = {
+                                         site : {
+                                         syncUrl : "http://sync.couchbasecloud.com/registration/",
+                                         db : db,
+                                         s : coax(url),
+                                         info : info,
+                                         views : views,
+                                         server : url
+                                         
+                                         }
+                                         
+                                
+                                         }
+                                         console.log("setting window.config");
+                                console.log("config", window.config.site.syncUrl)
+                                         registrationRequest('lorin@adobe.com','lorin');
+                                        });
+            
+                                });
+  
+                        console.log("this doesn't Work?");
+                                });
+        
     },
     
     // Update DOM on a Received Event
@@ -64,3 +91,94 @@ var app = {
         
     }
 };
+/*
+function pagenav(page) {
+    console.log("pagenav");
+    /*
+    $(currentpage).removeClass('active');
+    $(currentpage).addClass('inactive');
+    
+    $(page).addClass('active');
+    $(page).removeClass('inactive');
+     
+}
+*/
+
+
+var id;
+
+function registrationRequest(user,name) {
+ 
+    var doc = {};
+    doc.type = "registration";
+    doc.user = user;
+    doc.name = name;
+    doc.channels = "new-users";
+    console.log("hellohello");
+ 
+    
+    
+    window.config.site.db.post(doc, function(err, ok) {
+                   console.log("DB POST RESPONSE",JSON.stringify(err), JSON.stringify(ok));
+                               id = ok.id;
+                               dbquery(user,name);
+    });
+    
+   
+}
+
+function dbquery(user,name) {
+    console.log("DB QUERY LEVEL");
+    config.site.db(id, function(err, doc){
+                   console.log("we did it", doc.name);
+                   });
+}
+
+
+
+
+/*
+function jsonform(elem) {
+    var o = {}, list = $(elem).serializeArray();
+    for (var i = list.length - 1; i >= 0; i--) {
+        var name = list[i].name, value = list[i].value;
+        if (o[name]) {
+            if (!o[name].push) {
+                o[name] = [o[name]];
+            }
+            o[name].push(value);
+        } else {
+            o[name] = value;
+        }
+    };
+    return o;
+};
+
+*/
+
+function setupDb(db, cb) {
+    // db.del(function(){
+    db.put(function(){
+           db.get(cb)
+           })
+    // })
+}
+
+function setupViews(db, cb) {
+    var design = "_design/todo9"
+    db.put(design, {
+           views : {
+            lists : {
+                map : function(doc) {
+                    if (doc.type == "registration" && doc.user && doc.name) {
+                        emit(doc.name)
+                    }
+                    }.toString()
+            }
+           
+           }
+           }, function(){
+           cb(false, db([design, "_view"]))
+           })
+}
+
